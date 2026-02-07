05ae39640277751b0fc5529bcc692f0dc3411425
Ani <108333567+not-ani@users.noreply.github.com>
2026-02-07 14:12:19 -0700

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@elevated-school/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
